f018300e1f007441647919ca59857a80ecdcfe81
thorb3n1234 <thorben.kompalla@googlemail.com>
2020-07-09 08:56:35 +0200

    train

--- ML_DATA/custom_files/create_train_test_txt.py
+++ ML_DATA/custom_files/create_train_test_txt.py
@@ -2,18 +2,18 @@ import os
 import sys
 
 
-a = open("train.txt", "w")
-for file in os.listdir("../data/data/train/"):
+a = open("train_bottom.txt", "w")
+for file in os.listdir("../bottom/train/"):
 	if file.endswith('.jpg'):
-		a.write("../daten/ML_DATA/data/data/train/"+ str(file) + os.linesep)
+		a.write("../daten/ML_DATA/bottom/train/"+ str(file) + os.linesep)
 		#a.write("../GIT_DATA/ML_DATA/data/data/train/"+ str(file) + os.linesep)
 
 a.close()
 
 b = open("test_bottom.txt", "w")
-for file in os.listdir("../data/data/test/"):
+for file in os.listdir("../bottom/test/"):
 	if file.endswith('.jpg'):
-		b.write("../daten/ML_DATA/data/data/test/"+ str(file) + os.linesep)
+		b.write("../daten/ML_DATA/bottom/test/"+ str(file) + os.linesep)
 		#b.write("../GIT_DATA/ML_DATA/data/data/test/"+ str(file) + os.linesep)
 b.close()
 
