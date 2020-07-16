b3b0c2930534bf0cb57762db61bcacb52278b275
thorb3n1234 <thorben.kompalla@googlemail.com>
2020-07-16 11:47:18 +0200

    train und testing

--- ML_DATA/custom_files/create_train_test_txt.py
+++ ML_DATA/custom_files/create_train_test_txt.py
@@ -2,20 +2,20 @@ import os
 import sys
 
 
-#a = open("train.txt", "w")
-a = open("train_bottom.txt", "w")
+a = open("train.txt", "w")
+#a = open("train_bottom.txt", "w")
 for file in os.listdir("../data/data/train/"):
 	if file.endswith('.jpg'):
-		a.write("../daten/ML_DATA/bottom/train/"+ str(file) + os.linesep)
 		a.write("../daten/ML_DATA/data/data/train/"+ str(file) + os.linesep)
+		#a.write("../daten/ML_DATA/data/data/train/"+ str(file) + os.linesep)
 
 a.close()
 
-#b = open("test.txt", "w")
-b = open("test_bottom.txt", "w")
+b = open("test.txt", "w")
+#b = open("test_bottom.txt", "w")
 for file in os.listdir("../data/data/test/"):
 	if file.endswith('.jpg'):
-		b.write("../daten/ML_DATA/bottom/test/"+ str(file) + os.linesep)
 		b.write("../daten/ML_DATA/data/data/test/"+ str(file) + os.linesep)
+		#b.write("../daten/ML_DATA/data/data/test/"+ str(file) + os.linesep)
 b.close()
 
