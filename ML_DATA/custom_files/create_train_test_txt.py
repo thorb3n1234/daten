fd1bd9191d9bcb4a43fda60638daabc4c8cd40f7
thorb3n1234 <thorben.kompalla@googlemail.com>
2020-07-17 10:52:11 +0200

    rm negatives and *0.1learning rate

--- ML_DATA/custom_files/create_train_test_txt.py
+++ ML_DATA/custom_files/create_train_test_txt.py
@@ -1,21 +1,36 @@
 import os
 import sys
 
+#CONNECTION
+a = open("train.txt", "w")
+for file in os.listdir("../data/data/train/"):
+	if file.endswith('.jpg'):
+		a.write("../daten/ML_DATA/data/data/train/"+ str(file) + os.linesep)
 
-#a = open("train.txt", "w")
+a.close()
+
+
+b = open("test.txt", "w")
+for file in os.listdir("../data/data/test/"):
+	if file.endswith('.jpg'):
+		b.write("../daten/ML_DATA/data/data/test/"+ str(file) + os.linesep)
+b.close()
+
+
+
+#BOTTOM
+
+
 a = open("train_bottom.txt", "w")
 for file in os.listdir("../bottom/train/"):
 	if file.endswith('.jpg'):
 		a.write("../daten/ML_DATA/bottom/train/"+ str(file) + os.linesep)
-		#a.write("../daten/ML_DATA/data/data/train/"+ str(file) + os.linesep)
 
 a.close()
 
-#b = open("test.txt", "w")
+
 b = open("test_bottom.txt", "w")
 for file in os.listdir("../bottom/test/"):
 	if file.endswith('.jpg'):
 		b.write("../daten/ML_DATA/bottom/test/"+ str(file) + os.linesep)
-		#b.write("../daten/ML_DATA/data/data/test/"+ str(file) + os.linesep)
 b.close()
-
